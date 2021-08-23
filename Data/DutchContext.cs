445723d3d6c39fec619916d3fc7e422923cb041f
Jakub Mezey <jakub.mezey@outlook.com>
2021-08-23 20:21:36 +0200

    fix dbcontext

--- Data/DutchContext.cs
+++ Data/DutchContext.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 
