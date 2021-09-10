59f37b0fc64bdbcaafcb729d87b05b42ef0c6c10
Jan M <feralsvk456@gmail.com>
2021-09-10 12:31:16 +0200

    tyerjfg

--- Data/DutchSeeder.cs
+++ Data/DutchSeeder.cs
@@ -7,6 +7,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using DutchTreat.Data.Entities;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 
 namespace DutchTreat.Data
 {
@@ -14,16 +15,36 @@ namespace DutchTreat.Data
   {
     private readonly DutchContext _ctx;
     private readonly IWebHostEnvironment _hosting;
+        private readonly UserManager<StoreUser> _userManager;
 
-    public DutchSeeder(DutchContext ctx, IWebHostEnvironment hosting)
+        public DutchSeeder(DutchContext ctx, IWebHostEnvironment hosting, UserManager<StoreUser> userManager)
     {
       _ctx = ctx;
       _hosting = hosting;
+      _userManager = userManager;
     }
 
-    public void Seed()
+    public async Task SeedAsync()
     {
       _ctx.Database.EnsureCreated();
+
+            StoreUser user = await _userManager.FindByEmailAsync("feralsvk456@gmail.com");
+            if (user == null)
+            {
+                user = new StoreUser()
+                {
+                    FirstName = "lolol",
+                    LastName = "okokok",
+                    Email = "feralsvk456@gmail.com",
+                    UserName = "shawn@dutchtreat.com"
+                };
+
+                var result = await _userManager.CreateAsync(user, "P@ssw0rd!");
+                if (result != IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Nejde spravit novy user v seederi");
+                }
+            }
 
       if (!_ctx.Products.Any())
       {
@@ -36,6 +57,7 @@ namespace DutchTreat.Data
         var order = _ctx.Orders.Where(o => o.Id == 1).FirstOrDefault();
         if (order != null)
         {
+                    order.User = user;
           order.Items  = new List<OrderItem>()
           {
             new OrderItem()
